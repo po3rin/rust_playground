d5abc7efb09296129d47be889482e581295b639f
po3rin <abctail30@gmail.com>
2019-07-08 10:35:25 +0900

    ownership

--- try-syntax/src/main.rs
+++ try-syntax/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let v2 = vec![1, 2, 3];
 
     let answer = foo(&v1, &v2);
-    println!("{}", answer);
+    println!("answer {}", answer);
 
     // &mut参照
     let mut x = 5;
@@ -43,6 +43,10 @@ fn main() {
     *mm = 10;
     println!("mm is: {}", mm);
 
+    // ownership
+    string();
+    ownership()
+
 }
 
 fn foo(v1: &Vec<i32>, v2: &Vec<i32>) -> i32 {
@@ -52,3 +56,37 @@ fn foo(v1: &Vec<i32>, v2: &Vec<i32>) -> i32 {
     v1[0] + v2[1]
 }
 
+fn string(){
+    let mut s = String::from("hello");
+    s.push_str(", world!"); // push_str()関数は、リテラルをStringに付け加える
+    println!("{}", s); // これは`hello, world!`と出力する
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
+}
+
+fn ownership() {
+    let s1 = gives_ownership();         // gives_ownershipは、戻り値をs1に
+                                        // ムーブする
+    let s2 = String::from("hello");     // s2がスコープに入る
+
+    let s3 = takes_and_gives_back(s2);  // s2はtakes_and_gives_backにムーブされ
+                                        // 戻り値もs3にムーブされる
+} // ここで、s3はスコープを抜け、ドロップされる。s2もスコープを抜けるが、ムーブされているので、
+  // 何も起きない。s1もスコープを抜け、ドロップされる。
+
+fn gives_ownership() -> String {             // gives_ownershipは、戻り値を
+                                             // 呼び出した関数にムーブする
+
+    let some_string = String::from("hello"); // some_stringがスコープに入る
+
+    some_string                              // some_stringが返され、呼び出し元関数に
+                                             // ムーブされる
+}
+
+// takes_and_gives_backは、Stringを一つ受け取り、返す。
+fn takes_and_gives_back(a_string: String) -> String { // a_stringがスコープに入る。
+
+    a_string  // a_stringが返され、呼び出し元関数にムーブされる
+}
