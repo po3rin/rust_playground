dadedc038c47800bc777cc5c359e172d8898df37
po3rin <abctail30@gmail.com>
2019-07-20 02:44:45 +0900

    add args

---
+++ minigrep/src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    // {}を探しています
+    println!("Searching for {}", query);
+    // {}というファイルの中
+    println!("In file {}", filename);
+
+    // --snip--
+    println!("In file {}", filename);
+
+    // ファイルが見つかりませんでした
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        // ファイルの読み込み中に問題がありました
+        .expect("something went wrong reading the file");
+
+    // テキストは\n{}です
+    println!("With text:\n{}", contents);
+}
